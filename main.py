9a768b80e22d82117ff729e38b166cfaf8a9e04f
Christen Sebastian <Sebastian.Christen@swisscom.com>
2024-08-20 10:55:13 +0200

    feat: implement get methods

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {
-        "data": 
+
+
+
+@app.get('/blog')
+# Hier sind "Bool" und "10" die Default values, falls der Parameter nicht mitgegeben wurde im request.
+# Mit "Optional" kann mann angeben, wenn ein Parameter Optional ist, den Datentyp und default = None
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+
+    if published:
+        return {
+            "data": 
             {
-                "name": "Sebu"
-            }
+                    f"{limit} {sort} PUBLISHED blogs fetched from List",
+                
+        }}
+    else:
+      return {
+            "data": 
+            {
+                    f"{limit} {sort}  unpublished blogs fetched from List",
+                
+        }
+          
     } 
 
 
@@ -19,13 +36,39 @@ def index():
 
 
 
+@app.get('/blog/unpublished')
+def unpublished():
+    return {
+        "data": 
+            
+                "All unpublished blogs"
+            
+    } 
 
 
-@app.get('/about')
-def about():
+
+
+
+
+@app.get('/blog/{id}')
+def show(id: int):
     return {
         "data": 
             {
-                "site": "about"
+                "data": id
+            }
+    } 
+
+
+
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit = 10):
+    return {
+        "data": 
+            {
+                "data": f"id: {id}, limit: {limit}"
             }
     } 
